8fd901658b643d6ae85b669ec346f52884f293f2
PicuBicu <championello@gmail.com>
2021-12-28 23:41:00 +0100

    Change delete from basket function

--- pizzeria/basket.php
+++ pizzeria/basket.php
@@ -7,67 +7,54 @@ if (!isset($_SESSION["loggedin"])) {
 }
 
 try {
-
-    $sql = "SELECT f.id, f.name, m.price AS mała, s.price AS średnia, d.price AS duża, skl.ingredients
-    FROM basket, food AS f,
+    $sql = "SELECT food_size.id, f.name, m.price AS mała, s.price AS średnia, d.price AS duża, skl.ingredients FROM basket, food_size, food AS f, 
     (SELECT food_id, price FROM food_size WHERE name='mała') AS m, 
-    (SELECT food_id, price FROM food_size WHERE name='średnia') AS s,
-    (SELECT food_id, price FROM food_size WHERE name='duża') AS d,
-    (SELECT f.id AS x, f.name, GROUP_CONCAT(i.name SEPARATOR ', ') 
-    AS ingredients FROM food AS f, ingredients AS i, storage AS s 
-    WHERE s.food_id = f.id AND s.ingredient_id = i.id 
-    GROUP by f.id) AS skl
-    WHERE f.id = m.food_id
-    AND f.id = s.food_id
-    AND f.id = d.food_id
-    AND f.id = skl.x
-    AND basket.food_id = f.id
-    AND basket.client_id = :clientId";
-    if ($stmt = $pdo->prepare($sql)) {
+    (SELECT food_id, price FROM food_size WHERE name='średnia') AS s, 
+    (SELECT food_id, price FROM food_size WHERE name='duża') AS d, 
+    (SELECT f.id AS x, f.name, GROUP_CONCAT(i.name SEPARATOR ', ') AS ingredients 
+    FROM food AS f, ingredients AS i, storage AS s 
+    WHERE s.food_id = f.id 
+    AND s.ingredient_id = i.id 
+    GROUP by f.id) AS skl 
+    WHERE f.id = m.food_id 
+    AND f.id = s.food_id 
+    AND f.id = d.food_id 
+    AND f.id = skl.x 
+    AND food_size.food_id = f.id 
+    AND basket.client_id = :clientId 
+    AND basket.food_size_id = food_size.id";
+?>
+    <?php if ($stmt = $pdo->prepare($sql)) {
         $clientId = $_SESSION["clientId"];
         $stmt->bindParam(":clientId", $clientId, PDO::PARAM_INT);
-        if ($stmt->execute()) {
-            echo
-            "<h3>Zamówienie</h3>
-            <form action='order.php' method='post'>
+        if ($stmt->execute()) :
+    ?>
+            <h3>Zamówienie</h3>
+            <form action="order.php" method="post">
+                <ol class="list-group list-group-numbered">
+                    <?php if ($stmt->rowCount() > 0) : ?>
+                        <?php while ($row = $stmt->fetch()) : ?>
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                    <div class="fw-bold"><?php echo $row["name"] ?></div>
+                                    <div><?php echo $row["ingredients"] ?></div>
+                                </div>
+                                <label class="form-label" for="quantity">Ilość: </label>
+                                <input class="form-control" type="number" id="quantity" name="quantity[]" min="1" max="5" value="1">
+
+                                <button type="button" class="btn btn-danger"><a href="delete_from_basket.php?foodSizeId=<?php echo $row["id"] ?>">Usuń</a></button>
+                            </li>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+                </ol>
             </form>
-            <ol class='list-group list-group-numbered'>";
-            if ($stmt->rowCount() > 0) {
-                while ($row = $stmt->fetch()) {
-                    $id = $row['id'];
-                    $name = $row['name'];
-                    $small = $row['mała'];
-                    $medium = $row['średnia'];
-                    $large = $row['duża'];
-                    $ingredients = $row["ingredients"];
-                    echo
-                    "<li class='list-group-item d-flex justify-content-between align-items-start'>
-                        <div class='ms-2 me-auto'>
-                            <div class='fw-bold'>$name</div>
-                            <div>$ingredients</div>
-                        </div>";
-                    echo
-                    "<label for='quantity'>Ilość: </label>
-                    <input class='form-label' type='number' id='quantity' name='quantity[]' min='1' max='5' value='1'>";
-                    echo
-                    "<div class='col-xs-2'>
-                        <select class='form-select form-select-sm mb-12' name='size[]'>
-                            <option value='mała'>Mała - $small zł</option>
-                            <option selected value='średnia'>Średnia - $medium zł</option>
-                            <option value='duża'>Duża - $large zł</option>;
-                        <select>
-                    </div>
-                    <button type='button' class='btn btn-danger'><a href='../delete_from_basket.php?foodId=$id'>Usuń</a></button>
-                    </li>";
-                }
-                echo "</ol>";
-            } else {
-                echo "<p>Brak wybranych produktów<p>";
-            }
-        }
+    <?php endif;
         unset($stmt);
-    }
+    } ?>
+<?php
     unset($pdo);
 } catch (PDOException $exp) {
+    echo $exp->getMessage();
     echo "Coś poszło nie tak ... Spróbuj ponownie później";
 }
+?>
--- pizzeria/delete_from_basket.php
+++ pizzeria/delete_from_basket.php
@@ -1,23 +1,24 @@
 <?php
 
-require_once 'config.php';
+require_once "config.php";
 
 session_start();
 
-if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+if (!isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     header("location: login.php");
 }
 
 try {
-    if ($_GET['foodId']) {
-        $foodId = $_GET['foodId'];
-        $clientId = $_SESSION['clientId'];
-        $sql = 'DELETE FROM basket WHERE client_id = :clientId AND food_id = :foodId';
+    if (isset($_GET["foodSizeId"])) {
+        echo $_GET["foodSizeId"];
+        $foodSizeId = $_GET["foodSizeId"];
+        $clientId = $_SESSION["clientId"];
+        $sql = "DELETE FROM basket WHERE basket.client_id = :clientId AND basket.food_size_id = :foodSizeId";
         if ($stmt = $pdo->prepare($sql)) {
-            $stmt->bindParam(':clientId', $clientId, PDO::PARAM_INT);
-            $stmt->bindParam(':foodId', $foodId, PDO::PARAM_INT);
+            $stmt->bindParam(":clientId", $clientId, PDO::PARAM_INT);
+            $stmt->bindParam(":foodSizeId", $foodSizeId, PDO::PARAM_INT);
             if ($stmt->execute()) {
-                header('location: orders.php');
+                header("location: orders.php");
             } else {
                 echo "Coś poszło nie tak ... Spróbuj ponownie później";
             }
