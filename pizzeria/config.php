54afa3630b5b5030d272c017fb49f95ed408355e
PicuBicu <championello@gmail.com>
2021-12-28 17:15:13 +0100

    Change database exception error message

--- pizzeria/config.php
+++ pizzeria/config.php
@@ -9,5 +9,5 @@ try {
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Error: Could not connect " . $e->getMessage());
+    die("Database is not available right now, please try again later");
 }
