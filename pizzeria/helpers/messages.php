7a6e41e713ca517314ed4ca40f44d586a92685b7
PicuBicu <championello@gmail.com>
2021-12-29 17:43:24 +0100

    Add few application messages

---
+++ pizzeria/helpers/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+define("ADD_TO_BASKET_ERROR", "Wystąpił błąd podczas wstawiania elementu do koszyka");
+define("ADD_TO_BASKET_SUCCESS", "Pomyślnie dodano produkt do koszyka");
+define("PRODUCT_ALREADY_IN_BASKET", "Podany produkt znajduje się już w koszyku");
+define("CANNOT_FIND_PRODUKT", "Nie posiadamy podanego produktu");
+define("DATABASE_EXCEPTION", "Bład przetwarzania bazy danych, spróbuj ponownie później");
+define("CANNOT_PROCESS_USERS_BASKET", "Aktualnie nie możemy wyświetlić produktów w koszyku, przepraszamy");
+define("DELETE_FROM_BASKET_SUCCESS", "Pomyślnie usunięto produkt z koszyka");
+define("DELETE_FROM_BASKET_ERROR", "Wystąpił błąd podczas wstawiania elementu do koszyka");
