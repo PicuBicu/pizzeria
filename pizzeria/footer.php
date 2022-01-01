8195b1ec1fb869b2a801fa6136852e5a6e4546f6
PicuBicu <championello@gmail.com>
2022-01-01 16:26:17 +0100

    Update footer year and add top border

--- pizzeria/footer.php
+++ pizzeria/footer.php
@@ -1,7 +1,10 @@
 </main>
-<footer>
-    <span class="placeholder" style="width: 100%;"></span>
-    <p class="text-center">&copy; 2021 Pizzeria, Inc</p>
+<footer class="border-top mt-2">
+    <p class="text-center">
+        <script>
+            document.write(`&copy; ${new Date().getFullYear()} Pizzeria, Inc`)
+        </script>
+    </p>
 </footer>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
