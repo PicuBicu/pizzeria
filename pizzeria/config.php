886e9518ce156ce0b47e7ca1bb151f88dde3ca25
PicuBicu <championello@gmail.com>
2022-01-06 14:46:01 +0100

    Display error page while db exception

--- pizzeria/config.php
+++ pizzeria/config.php
@@ -13,7 +13,9 @@ try {
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    // TODO: header to special error page
+    require "header.php";
     setAlertInfo(DATABASE_EXCEPTION, DANGER);
+    require "helpers/alert.php";
+    require "footer.php";
     exit();
 }
