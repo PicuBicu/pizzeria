76c6db689202dc914c12114d95b34b1a4c17e171
PicuBicu <championello@gmail.com>
2022-01-08 16:22:54 +0100

    Add checkPasswordStrenth

--- pizzeria/helpers/utils.php
+++ pizzeria/helpers/utils.php
@@ -64,3 +64,17 @@ function redirectIfNotEnoughPermisions()
         goToLocationWithWarning("location: ../menu.php", PERMISSION_DENIED);
     }
 }
+
+function checkPasswordStrenth($password)
+{
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number    = preg_match('@[0-9]@', $password);
+    $specialChars = preg_match('@[^\w]@', $password);
+
+    if (!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+        return false;
+    } else {
+        return true;
+    }
+}
