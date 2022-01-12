7e08c9e49b4c2b15b74d281173d0f2f54a1a9358
PicuBicu <championello@gmail.com>
2022-01-12 21:41:26 +0100

    Make all fields in add address form smaller

--- pizzeria/views/address-add-form.php
+++ pizzeria/views/address-add-form.php
@@ -1,10 +1,16 @@
 <form action="controllers/address_add.php" method="post">
     <h2>Wprowadź adres dostawy</h2>
-    <label for="street" class="mt-2">Ulica:</label>
-    <input type="text" name="street" class="form-control" />
-    <label for="houseNumber" class="mt-2">Numer domu:</label>
-    <input type="text" name="houseNumber" class="form-control" />
-    <label for="city" class="mt-2">Miejscowość:</label>
-    <input type="text" name="city" class="form-control" />
+    <div class="col-4">
+        <label for="street" class="mt-2">Ulica:</label>
+        <input type="text" name="street" class="form-control col-4" />
+    </div>
+    <div class="col-4">
+        <label for="houseNumber" class="mt-2">Numer domu:</label>
+        <input type="text" name="houseNumber" class="form-control col-4" />
+    </div>
+    <div class="col-4">
+        <label for="city" class="mt-2">Miejscowość:</label>
+        <input type="text" name="city" class="form-control col-4" />
+    </div>
     <button type="submit" class="btn btn-primary mt-2">Zapisz adres</button>
 </form>
