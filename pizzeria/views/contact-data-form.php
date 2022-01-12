66ab9d1aabc04386eb509a9840ab53ca5d086a4d
PicuBicu <championello@gmail.com>
2022-01-12 21:43:44 +0100

    Make fields in add contact data form smaller

--- pizzeria/views/contact-data-form.php
+++ pizzeria/views/contact-data-form.php
@@ -1,8 +1,12 @@
 <form action="controllers/contact_data_add.php" method="post">
     <h2>Wprowadź dane kontaktowe</h2>
-    <label for="email" class="mt-2">Email:</label>
-    <input type="text" name="email" class="form-control" />
-    <label for="phoneNumber" class="mt-2">Numer telefonu:</label>
-    <input type="text" name="phoneNumber" class="form-control" />
+    <div class="col-4">
+        <label for="email" class="mt-2">Email:</label>
+        <input type="text" name="email" class="form-control" />
+    </div>
+    <div class="col-4">
+        <label for="phoneNumber" class="mt-2">Numer telefonu:</label>
+        <input type="text" name="phoneNumber" class="form-control" />
+    </div>
     <button type="submit" class="btn btn-primary mt-2">Zapisz dane kontaktowe</button>
 </form>
