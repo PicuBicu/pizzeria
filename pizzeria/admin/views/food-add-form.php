589799140b0450859ea164f327f3aa2d88f9b486
PicuBicu <championello@gmail.com>
2022-01-13 20:26:09 +0100

    Change width of fields in food add form

--- pizzeria/admin/views/food-add-form.php
+++ pizzeria/admin/views/food-add-form.php
@@ -1,14 +1,24 @@
 <form action="controllers/product_add.php" method="post" enctype="multipart/form-data">
-    <label for="name" class="mt-2">Nazwa produktu:</label>
-    <input class="form-control" required name="name" />
-    <label for="small" class="mt-2">Cena za rozmiar mały:</label>
-    <input class="form-control" type="number" required name="small" />
-    <label for="average" class="mt-2">Cena za rozmiar średni:</label>
-    <input class="form-control" type="number" required name="average" />
-    <label for="big" class="mt-2">Cena za rozmiar duży:</label>
-    <input class="form-control" type="number" required name="big" />
-    <label for="photo" class="mt-2">Dodaj obrazek</label>
-    <input class="form-control" type="file" required name="uploadFile" value="" />
+    <div class="col-4">
+        <label for="name" class="mt-2">Nazwa produktu:</label>
+        <input class="form-control" required name="name" />
+    </div>
+    <div class="col-4">
+        <label for="small" class="mt-2">Cena za rozmiar mały:</label>
+        <input class="form-control" type="number" required name="small" />
+    </div>
+    <div class="col-4">
+        <label for="average" class="mt-2">Cena za rozmiar średni:</label>
+        <input class="form-control" type="number" required name="average" />
+    </div>
+    <div class="col-4">
+        <label for="big" class="mt-2">Cena za rozmiar duży:</label>
+        <input class="form-control" type="number" required name="big" />
+    </div>
+    <div class="col-4">
+        <label for="photo" class="mt-2">Dodaj obrazek</label>
+        <input class="form-control" type="file" required name="uploadFile" value="" />
+    </div>
     <div class=" mt-2">
         <?php foreach ($ingredientsList as $row) : ?>
             <input type="checkbox" class="form-check-input" name="ingredients[]" value="<?= $row["id"] ?>" />
