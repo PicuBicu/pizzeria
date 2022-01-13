454f90d25e8b0574ffd65d4d44a06e89d1c3cefd
PicuBicu <championello@gmail.com>
2022-01-13 20:23:23 +0100

    Change address select form fields width

--- pizzeria/views/address-select-form.php
+++ pizzeria/views/address-select-form.php
@@ -1,13 +1,19 @@
 <form action="controllers/order_make.php" method="post">
-    <label for="addressId" class="mt-2">Wybierz adres dostawy:</label>
-    <select name="addressId" class="form-control">
-        <?php foreach ($addressesList as $row) : ?>
-            <option value="<?= $row["id"] ?>"><?= "Ulica: " . $row["street"] . " Numer domu: " . $row["house_number"] . " Miasto: " . $row["city"] ?></option>
-        <?php endforeach; ?>
-    </select>
-    <?php require_once "views/contact-data-select-form.php" ?>
-    <label for="informationForCourier" class="mt-2">Informacje dla kuriera:</label>
-    <textarea class="form-control mb-2" name="informationForCourier" placeholder="Max 255 znaków"></textarea>
+    <div class="col-6">
+        <label for="addressId" class="mt-2">Wybierz adres dostawy:</label>
+        <select name="addressId" class="form-control">
+            <?php foreach ($addressesList as $row) : ?>
+                <option value="<?= $row["id"] ?>"><?= "Ulica: " . $row["street"] . " Numer domu: " . $row["house_number"] . " Miasto: " . $row["city"] ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+    <div class="col-6">
+        <?php require_once "views/contact-data-select-form.php" ?>
+    </div>
+    <div class="col-6">
+        <label for="informationForCourier" class="mt-2">Informacje dla kuriera:</label>
+        <textarea class="form-control mb-2" name="informationForCourier" placeholder="Max 255 znaków"></textarea>
+    </div>
     <input type="checkbox" name="confirmation" class="form-check-input" />
     <label for="confirmation">Czy przesłać potwierdzenie na maila?</label>
     <div class="hr"></div>
