680b4096d88b91f1fdb3c1206cbb072d46787c80
PicuBicu <championello@gmail.com>
2022-01-13 20:27:32 +0100

    Change width of fields in food update form

--- pizzeria/admin/views/food-update-form.php
+++ pizzeria/admin/views/food-update-form.php
@@ -1,14 +1,24 @@
 <form action="controllers/product_update.php?foodId=<?= $_GET["foodId"] ?>" method="post" enctype="multipart/form-data">
-    <label for="name" class="mt-2">Nazwa produktu:</label>
-    <input class="form-control" name="name" value="<?= $food["name"] ?>" placeholder="<?= $food["name"] ?>" />
-    <label for="small" class="mt-2">Cena za rozmiar mały:</label>
-    <input class="form-control" required type="number" name="small" value="<?= $food["mała"] ?>" placeholder="<?= $food["mała"] ?>" />
-    <label for="average" class="mt-2">Cena za rozmiar średni:</label>
-    <input class="form-control" required type="number" name="average" value="<?= $food["średnia"] ?>" placeholder="<?= $food["średnia"] ?>" />
-    <label for="big" class="mt-2">Cena za rozmiar duży:</label>
-    <input class="form-control" required type="number" name="big" value="<?= $food["duża"] ?>" placeholder="<?= $food["duża"] ?>" />
-    <label for="photo" class="mt-2">Dodaj obrazek</label>
-    <input class="form-control" type="file" name="uploadFile" />
+    <div class="col-4">
+        <label for="name" class="mt-2">Nazwa produktu:</label>
+        <input class="form-control" name="name" value="<?= $food["name"] ?>" placeholder="<?= $food["name"] ?>" />
+    </div>
+    <div class="col-4">
+        <label for="small" class="mt-2">Cena za rozmiar mały:</label>
+        <input class="form-control" required type="number" name="small" value="<?= $food["mała"] ?>" placeholder="<?= $food["mała"] ?>" />
+    </div>
+    <div class="col-4">
+        <label for="average" class="mt-2">Cena za rozmiar średni:</label>
+        <input class="form-control" required type="number" name="average" value="<?= $food["średnia"] ?>" placeholder="<?= $food["średnia"] ?>" />
+    </div>
+    <div class="col-4">
+        <label for="big" class="mt-2">Cena za rozmiar duży:</label>
+        <input class="form-control" required type="number" name="big" value="<?= $food["duża"] ?>" placeholder="<?= $food["duża"] ?>" />
+    </div>
+    <div class="col-4">
+        <label for="photo" class="mt-2">Dodaj obrazek</label>
+        <input class="form-control" type="file" name="uploadFile" />
+    </div>
     <div class=" mt-2">
         <?php foreach ($ingredientsList as $row) : ?>
             <div>
