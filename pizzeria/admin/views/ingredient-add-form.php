b23936986e2bb3077a91c022ebba779909d3acd1
PicuBicu <championello@gmail.com>
2022-01-13 20:28:49 +0100

    Change width of field in ingredient add form

--- pizzeria/admin/views/ingredient-add-form.php
+++ pizzeria/admin/views/ingredient-add-form.php
@@ -1,5 +1,7 @@
 <form action="controllers/ingredient_add.php" method="post">
-    <label for="name" class="mt-2">Nazwa składnika:</label>
-    <input class="form-control" required name="name" value="" />
+    <div class="col-4">
+        <label for="name" class="mt-2">Nazwa składnika:</label>
+        <input class="form-control" required name="name" value="" />
+    </div>
     <button type="submit" class="btn btn-primary mt-2">Dodaj składnik</button>
 </form>
