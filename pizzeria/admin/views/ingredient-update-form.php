49b5586dc0e853ff36652f530718d5332b112998
PicuBicu <championello@gmail.com>
2022-01-13 20:29:25 +0100

    Change width of field in ingredient update form

--- pizzeria/admin/views/ingredient-update-form.php
+++ pizzeria/admin/views/ingredient-update-form.php
@@ -1,5 +1,7 @@
 <form action="controllers/ingredient_update.php?id=<?= $_GET["ingredientId"] ?>" method="post">
-    <label for="name" class="mt-2">Nazwa składnika:</label>
-    <input class="form-control" required name="name" placeholder="<?= $ingredientName ?>" />
+    <div class="col-4">
+        <label for="name" class="mt-2">Nazwa składnika:</label>
+        <input class="form-control" required name="name" placeholder="<?= $ingredientName ?>" />
+    </div>
     <button type="submit" class="btn btn-primary mt-2">Zmień nazwę</button>
 </form>
